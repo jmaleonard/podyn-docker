899d4ac6c0b1c73c5b0db79426915c52db6ab0db
Jared Leonard <jaredmyronarthurleonard@gmail.com>
2018-07-11 15:29:59 +0200

    added support for convert-numbers-to-text option

--- files/startscript.sh
+++ files/startscript.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-echo 'VERSION 0.0.1'
+echo 'VERSION 0.0.2'
 cd podyn
 CONNECTION="jdbc:postgresql://"${POSTGRES_HOST}":"${POSTGRES_PORT}"/"${DATABASE_NAME}"?sslmode=require&user="${DATABASE_USERNAME}"&password="${DATABASE_PASSWORD}""
 
-echo "Starting initial sync for table ${TABLE}"
-./podyn --postgres-jdbc-url $CONNECTION --schema --data --table "$TABLE"
-wait
-echo "Listening for changes for table ${TABLE}"
-./podyn --postgres-jdbc-url $CONNECTION --changes --table "$TABLE"
+if [ -n "${CONVERT_NUMBERS_TO_TEXT}" ]; then
+    echo "Converting number fields to text"
+    echo "Starting initial sync for table ${TABLE}"
+    ./podyn --postgres-jdbc-url $CONNECTION --schema --data --convert-numbers-to-text --table "$TABLE" 
+    wait
+    echo "Listening for changes for table ${TABLE}"
+    ./podyn --postgres-jdbc-url $CONNECTION --changes --convert-numbers-to-text --table "$TABLE" 
+else 
+    echo "Starting initial sync for table ${TABLE}"
+    ./podyn --postgres-jdbc-url $CONNECTION --schema --data --table "$TABLE" 
+    wait
+    echo "Listening for changes for table ${TABLE}"
+    ./podyn --postgres-jdbc-url $CONNECTION --changes --table "$TABLE" 
+fi
